863ca703571335a23b58c95a7059717ae273f8d5
김영진 <youngjin1293@naver.com>
2024-02-07 14:40:46 +0900

    캐릭터 랜더링,정렬,삭제 기능

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import mockItems from "./pokemons.json";
+import { useState } from "react";
+
+const Pokemon = ({ item, onDelete }) => {
+  const handleDeleteClick = () => {
+    onDelete(item.id);
+  };
+  return (
+    <div>
+      No.{item.id} {item.name}
+      <button onClick={handleDeleteClick}>삭제</button>
+    </div>
+  );
+};
 
 function App() {
+  const [items, setItems] = useState(mockItems);
+  const [direction, setDirection] = useState(1);
+
+  const handleAscClick = () => setDirection(1);
+  const handleDescClick = () => setDirection(-1);
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
+  const sortedItems = items.sort((a, b) => direction * (a.id - b.id));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={handleAscClick}>오름차순</button>
+        <button onClick={handleDescClick}>내림차순</button>
+      </div>
+      <ul>
+        {sortedItems.map((item) => (
+          <li key={item.id}>
+            <Pokemon item={item} onDelete={handleDelete} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
